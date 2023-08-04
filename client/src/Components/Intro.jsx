29128e3387b3c6516f71b613779c7b152956d85e
Eshwar Reddy <eshwarvallela18@gmail.com>
2023-08-04 23:38:24 +0530

    initail commit

---
+++ client/src/Components/Intro.jsx
@@ -0,0 +1,13 @@
+
+
+function Intro(){
+    return (
+    <div>
+        <h1> Track The Show</h1>
+        <h3> Place to track Movies, TV Shows, Anime, etc.</h3>
+        <button>Login</button>
+        <button>Sign Up</button>
+    </div>
+    )
+}
+export default Intro;
